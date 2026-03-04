2491b6e01b3ed4ee832808256ca8cf5828d2b2c4
Morshed Alam <65453251+morshedalamdev@users.noreply.github.com>
2026-03-04 08:13:30 +0000

    Add Computing_Circle class to calculate and display the area of a circle

---
+++ Computing_Circle.java
@@ -0,0 +1,11 @@
+
+public class Computing_Circle {
+    public static void main(String[] args) {
+        double radius, area;
+
+        radius = 5;
+        area = radius * radius * 3.14159;
+
+        System.out.println("The area of the circle with radius " + radius + " is " + area);
+    }
+}
