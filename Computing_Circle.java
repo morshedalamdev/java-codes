20de26d4f572a2b27ab1270f48e24e7f1c15dbfd
Morshed Alam <65453251+morshedalamdev@users.noreply.github.com>
2026-03-04 08:27:19 +0000

    Refactor Computing_Circle to accept user input for radius calculation

--- Computing_Circle.java
+++ Computing_Circle.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
 
 public class Computing_Circle {
     public static void main(String[] args) {
         double radius, area;
 
-        radius = 5;
-        area = radius * radius * 3.14159;
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter the radius of the circle: ");
+            radius = in.nextDouble();
+            
+            area = radius * radius * 3.14159;
 
-        System.out.println("The area of the circle with radius " + radius + " is " + area);
+            System.out.println("The area of the circle with radius " + radius + " is " + area);
+        }
     }
 }
